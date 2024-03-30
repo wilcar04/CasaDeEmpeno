c5c9ea2cb5703416b71c588559b76bc4261f5e37
JevDev2304 <jvaldes245@soyudemedellin.edu.co>
2024-03-30 14:19:56 -0500

    solving contract manager

--- src/ManagerContract.java
+++ src/ManagerContract.java
@@ -1,7 +1,7 @@
-import java.net.ConnectException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class ManagerContract {
     private List<Contract> contracts;
@@ -11,20 +11,28 @@ public class ManagerContract {
 
     }
 
-    public void createContract(Float interest, Date initial_format, Date deadline, Float valor, Item item, Client client){
-        Contract contract = new Contract(interest,initial_format,deadline,valor, item, client);
+    public List<Contract> getContracts() {
+        return contracts;
+    }
+
+    public void createContract(Float interest, Date initial_date, Date deadline, Float valor, Item item, Client client){
+        Contract contract = new Contract(interest,initial_date,deadline,valor, item, client);
         this.addNewContract(contract);
     }
     public void addNewContract(Contract contract){
         this.contracts.add(contract);
     }
 
-    public void findContract(int id ){
-        Contract findContract = this.contracts
+    public Contract findContract(int id ){
+        return this.contracts
                 .stream()
                 .filter(contract -> contract.id == id)
                 .toList()
                 .getFirst();
+    }
+
+    public boolean existContract (int id){
+        return this.getContracts().stream().anyMatch(contract -> contract.id == id);
     }
 
     public void updateContract(Contract contract){
@@ -41,13 +49,60 @@ public class ManagerContract {
                 } ).toList();
     }
 
+
     public void deleteContract(int id){
         this.contracts = this.contracts
                 .stream()
                 .filter(contract -> contract.id != id)
                 .toList();
+    }
+
+    public void changeStatus(int id, String status){
+        Contract findContract = this.findContract(id);
+        findContract.setState(status);
+
+    }
 
 
+    public List<Contract> getItemsWithDeadlineDateInOneWeek(){
+
+        return this.getContracts().stream()
+                .filter(Contract::checkDeadLineInOneWeek)
+                .toList();
+    }
+    public  List<Item> getItemsOfListContracts(List<Contract> contracts){
+        return this.getContracts().stream()
+                .map(contract -> contract.item)
+                .toList();
+    }
+
+    public void deleteExpiredContracts(){
+        this.contracts = this.contracts.stream()
+                .filter(contract -> contract.getState() != "expired" )
+                .toList();
+    }
+    public List<Item> getItemsOfExpiredContracts(){
+        List<Contract> expiredContracts = this.getExpiredContracts();
+        return this.getItemsOfListContracts(expiredContracts);
+
+
+    }
+    public List<Contract> getExpiredContracts() {
+        return this.getContracts()
+                .stream()
+                .filter(Contract::checkDeadLine)
+                .toList();
+    }
+    public List<Contract> getCurrentContracts(){
+        return getContractsByState("current");
+    }
+    public List<Contract> getPaidContracts(){
+        return getContractsByState("paid");
+    }
+    private  List<Contract> getContractsByState(String state){
+        return this.contracts.stream()
+                .filter(contract -> Objects.equals(contract.getState(), state))
+                .toList();
     }
 
 
