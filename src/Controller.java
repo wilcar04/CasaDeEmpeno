fd41b6ef9f59b429e0ee6e309832a33522f67928
Wilson <wilson.santiago.car@gmail.com>
2024-04-01 20:54:52 -0500

    first commit testing

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public class Controller {
-    private static Shop shop;
+    private static Shop shop = new Shop(0, "Tienda de Empeño");
     public static void mainLoop(){
         shop.loadInitialData();
         while(true){
